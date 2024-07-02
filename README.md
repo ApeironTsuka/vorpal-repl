5c05399fc1e8765af013dc4673a4b1833fa987b6
Apeiron <apeiron@none>
2024-07-02 16:42:17 -0500

    Initial commit after a review/refactoring and updating of dependencies.

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 # vorpal-repl
 
-[![Build Status](https://travis-ci.org/vorpaljs/vorpal-repl.svg)](https://travis-ci.org/vorpaljs/vorpal-repl)
-
-REPL extension for [Vorpal.js](https://github.com/dthree/vorpal). Installs the `repl` command, which drops you into a REPL session within the context of the application. Built in to Vantage.js by default.
+REPL extension for [Vorpal.js](https://github.com/ApeironTsuka/vorpal). Installs the `repl` command, which drops you into a REPL session within the context of the application. Built in to Vantage.js by default.
 
 ##### Installation
 
 ```bash
-npm install vorpal-repl
-npm install vorpal
+npm install @ApeironTsuka/vorpal-repl
+npm install @ApeironTsuka/vorpal
 ```
 
 ##### Programmatic use
 
 ```js
 // index.js
-const Vorpal = require('vorpal');
-const repl = require('vorpal-repl');
+import Vorpal from '@ApeironTsuka/vorpal';
+import repl from '@ApeironTsuka/vorpal-repl';
 
 const vorpal = Vorpal();
 
--- test/repl.js
+++ test/repl.js
@@ -1,13 +1,11 @@
 'use strict';
 
-require('assert');
+import assert from 'assert';
+import should from 'should';
+import Vantage from '@ApeironTsuka/vorpal';
+import repl from '../lib/repl.js';
 
-var should = require('should');
-var Vantage = require('vorpal');
-var repl = require('./../lib/repl');
-
-var vorpal;
-var stdout = '';
+let stdout = '', vorpal;
 
 global.foo = 'bar';
 global.blob = {
@@ -24,61 +22,61 @@ function stdoutFn(data) {
   return '';
 }
 
-describe('vorpal-repl', function () {
-  describe('root', function () {
-    before('vorpal preps', function () {
+describe('vorpal-repl', () => {
+  describe('root', () => {
+    before('vorpal preps', () => {
       vorpal = new Vantage().pipe(stdoutFn).show();
     });
 
-    beforeEach('vorpal preps', function () {
+    beforeEach('vorpal preps', () => {
       stdout = '';
     });
 
-    it('should exist and be a function', function () {
+    it('should exist and be a function', () => {
       should.exist(repl);
       repl.should.be.type('function');
     });
 
-    it('should import into Vantage', function () {
-      (function () {
+    it('should import into Vantage', () => {
+      (() => {
         vorpal.use(repl);
       }).should.not.throw();
     });
 
-    it('should log into REPL', function (done) {
-      vorpal.exec('repl', function (err, data) {
+    it('should log into REPL', (done) => {
+      vorpal.exec('repl', (err, data) => {
         should.not.exist(err);
         data.should.containEql('Entering REPL Mode.');
         done();
       });
     });
 
-    it('should execute JS', function (done) {
-      vorpal.exec('Math.pow(6 * 6, 2);', function (err, data) {
+    it('should execute JS', (done) => {
+      vorpal.exec('Math.pow(6 * 6, 2);', (err, data) => {
         should.not.exist(err);
         data.should.equal(1296);
         done();
       });
     });
 
-    it('should have access to global', function (done) {
-      vorpal.exec('foo', function (err, data) {
+    it('should have access to global', (done) => {
+      vorpal.exec('foo', (err, data) => {
         should.not.exist(err);
         data.should.equal('bar');
         done();
       });
     });
 
-    it('should stringify JSON', function (done) {
-      vorpal.exec('blob.json', function (err) {
+    it('should stringify JSON', (done) => {
+      vorpal.exec('blob.json', (err) => {
         should.not.exist(err);
         stdout.replace(/\n/g, '').replace(/\\/g, '').should.equal('{  "uncle": "sam",  "aunt": "mary"}');
         done();
       });
     });
 
-    it('should nail circular references like a boss', function (done) {
-      vorpal.exec('blob.circular', function (err) {
+    it('should nail circular references like a boss', (done) => {
+      vorpal.exec('blob.circular', (err) => {
         should.not.exist(err);
         done();
       });
